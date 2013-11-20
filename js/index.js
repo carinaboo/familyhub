e7d0e412d4c8045de473f23ca01162cf9e095c99
Carina <carinaboo@berkeley.edu>
2013-11-20 00:15:33 -0800

    button images

--- js/index.js
+++ js/index.js
@@ -1,22 +1,32 @@
 $(document).ready(function(){
 	$("#ok").click(function(){
-		if ($("#message").val()) {
-			sendText( $("#message").val() )
-		} else {
+		if ($("#message").val()) { // send a message and clear input
+			sendText( $("#message").val() );
+			$("#message").val("");
+		} else { //send ok
 			sendOk();
 		}
+
+		// go to bottom of page
+		window.scrollTo(0,document.body.scrollHeight);
 	})
 })
 
 function sendText(msg) {
-	$(".message-rows").append("<div class='row sent'> \
+	var html = "<div class='row sent'> \
     				<div class='triangle'></div> \
     				<div class='message'>" + msg + "</div> \
-    			</div>")
+    			</div>"
+	$(html).hide().appendTo(".message-rows").show();
 }
 
 function sendOk() {
-	var imgHTML = "<img src='images/ok.gif'>";
-	sendText(imgHTML);
+	var imgHTML = "<img class='ok-img' src='images/ok.gif'>";
+	var html = "<div class='row sent'> \
+    				<div class='triangle'></div> \
+    				<div class='message ok'>" + imgHTML + "</div> \
+    			</div>"
+
+	$(html).hide().appendTo(".message-rows").show();
 }
 
